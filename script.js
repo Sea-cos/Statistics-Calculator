680b89391e86d476d874f97399581adc7b17fc79
Marcos Vinicius <marcos_viny_12@outlook.com>
2024-07-04 12:00:28 -0300

    feat: created the getMode function

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-const calculate = () => {
-    const value = document.querySelector('#numbers').value
-    const array = value.split(/,\s*/g);
-    const numbers = array.map(el => Number(el)).filter(el => !isNaN(el))
-    const mean = getMean(numbers);
-    const median = getMedian(numbers);
-    document.querySelector("#mean").textContent = mean;
-    document.querySelector("#median").textContent = median;
-
-}
-
 const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
 
 const getMedian = (array) => {
@@ -22,4 +11,33 @@ const getMedian = (array) => {
         const odd = sorted[Math.floor(sorted.length / 2)];
         return odd;
     }
+}
+
+const getMode = (array) => {
+    const counts = {};
+    array.forEach((el) => {
+        counts[el] = (counts[el] || 0) + 1;
+    })
+    if (new Set(Object.values(counts)).size === 1) {
+        return null;
+    }
+    const highest = Object.keys(counts).sort(
+        (a, b) => counts[b] - counts[a]
+    )[0];
+    const mode = Object.keys(counts).filter(
+        (el) => counts[el] === counts[highest]
+    );
+    return mode.join(", ");
+}
+
+const calculate = () => {
+    const value = document.querySelector('#numbers').value
+    const array = value.split(/,\s*/g);
+    const numbers = array.map(el => Number(el)).filter(el => !isNaN(el))
+    const mean = getMean(numbers);
+    const median = getMedian(numbers);
+    const mode = getMode(numbers);
+    document.querySelector("#mean").textContent = mean;
+    document.querySelector("#median").textContent = median;
+    document.querySelector("#mode").textContent = mode;
 }
